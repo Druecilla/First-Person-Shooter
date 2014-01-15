5d58f2ebabc27b092f7adbe0f09898a669b50f12
bartbart333 <bartholdbos@hotmail.com>
2014-01-15 17:11:11 +0100

    Add FontRenderer

---
+++ src/nl/stefsiekman/fps/FontRenderer.java
@@ -0,0 +1,27 @@
+package nl.stefsiekman.fps;
+
+import java.awt.Font;
+
+import org.newdawn.slick.TrueTypeFont;
+
+public class FontRenderer {
+	
+	private static TrueTypeFont font;
+	
+	public FontRenderer(){
+		Font awt = new Font("Times", Font.PLAIN, 14);
+		font = new TrueTypeFont(awt, true);
+	}
+	
+	public static int getWidth(String text){
+		return font.getWidth(text);
+	}
+	
+	public static int getHeight(){
+		return font.getHeight();
+	}
+	
+	public static void drawString(int x, int y, String text, Color color){
+		font.drawString(x, y, text, new org.newdawn.slick.Color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
+	}
+}
