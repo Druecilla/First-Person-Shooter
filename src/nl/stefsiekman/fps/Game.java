5555dd8347029d8d0bec57798a3bbb0a3da25ccb
bartbart333 <bartholdbos@hotmail.com>
2014-01-16 21:21:29 +0100

    Add Options

--- src/nl/stefsiekman/fps/Game.java
+++ src/nl/stefsiekman/fps/Game.java
@@ -7,6 +7,7 @@ import javax.swing.JOptionPane;
 import nl.stefsiekman.fps.state.State;
 import nl.stefsiekman.fps.state.StateMainMenu;
 import nl.stefsiekman.fps.state.StateMultiplayerMenu;
+import nl.stefsiekman.fps.state.StateOptions;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
@@ -35,6 +36,7 @@ public class Game {
 		State.addState(State.States.MAIN_MENU, new StateMainMenu());
 		State.addState(State.States.MULTIPLAYER_MENU, new StateMultiplayerMenu());
 		State.addState(State.States.SINGLEPLAYER_MENU, new StateMultiplayerMenu());
+		State.addState(State.States.OPTIONS, new StateOptions());
 		State.setState(State.States.MAIN_MENU);
 		
 		while(!Display.isCloseRequested() && running){
---
+++ src/nl/stefsiekman/fps/state/StateOptions.java
@@ -0,0 +1,10 @@
+package nl.stefsiekman.fps.state;
+
+import nl.stefsiekman.fps.gui.MenuScreen;
+
+public class StateOptions extends MenuScreen{
+	
+	public StateOptions(){
+		
+	}
+}
