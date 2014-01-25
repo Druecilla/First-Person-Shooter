f844d4e7d959d5c0c9d1ab0b1b28a79bf1fc7136
bartbart333 <bartholdbos@hotmail.com>
2014-01-25 14:37:20 +0100

    Add StateLoadMap

--- src/nl/stefsiekman/fps/state/StateLoadMap.java
+++ src/nl/stefsiekman/fps/state/StateLoadMap.java
@@ -1,6 +1,9 @@
 package nl.stefsiekman.fps.state;
 
+import java.io.IOException;
+
 import nl.stefsiekman.fps.gui.MenuScreen;
+import nl.stefsiekman.fps.mapa.MapArchive;
 
 public class StateLoadMap extends MenuScreen{
 	
@@ -8,7 +11,11 @@ public class StateLoadMap extends MenuScreen{
 		
 	}
 	
-	public static void loadMap(String name){
-		State.setState(State.States.LOADMAP);
+	public void loadMap(MapArchive map){
+		try{
+			setBackgroundImage(map.getIntro());
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
