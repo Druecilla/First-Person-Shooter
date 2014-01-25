1d8fe4a9e3cff9727bf487a89c78db801db8ba55
SteFilms <s.siekman@gmail.com>
2014-01-25 13:27:02 +0100

    Mathf
    Create class for float math

---
+++ src/nl/stefsiekman/fps/math/Mathf.java
@@ -0,0 +1,12 @@
+package nl.stefsiekman.fps.math;
+
+public class Mathf {
+
+	/*
+	 * Returns the square root of the number given
+	 */
+	public static float sqrt(float f) {
+		return (float)Math.sqrt(f);
+	}
+	
+}
