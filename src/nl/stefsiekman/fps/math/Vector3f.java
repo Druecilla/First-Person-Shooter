ac7cdeed79c00e5388f9085d9f204f8ff1d51bd0
SteFilms <s.siekman@gmail.com>
2014-01-25 13:53:57 +0100

    Vector3f class
    Beginning of the Vector3f class

---
+++ src/nl/stefsiekman/fps/math/Vector3f.java
@@ -0,0 +1,47 @@
+package nl.stefsiekman.fps.math;
+
+public class Vector3f {
+
+	public float x;
+	public float y;
+	public float z;
+	
+	public Vector3f() {
+		this(0, 0, 0);
+	}
+
+	public Vector3f(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public float getLength() {
+		return Mathf.sqrt(x * x + y * y + z * z);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+	
+}
