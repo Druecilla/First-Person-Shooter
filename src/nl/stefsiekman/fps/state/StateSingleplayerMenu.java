29124e968eb1a780eb77f0159b1a203a242bd489
bartbart333 <bartholdbos@hotmail.com>
2014-01-25 14:48:56 +0100

    Clicking a map now starts the loading screen

--- src/nl/stefsiekman/fps/state/StateSingleplayerMenu.java
+++ src/nl/stefsiekman/fps/state/StateSingleplayerMenu.java
@@ -1,12 +1,15 @@
 package nl.stefsiekman.fps.state;
 
+import nl.stefsiekman.fps.LoadMap;
+import nl.stefsiekman.fps.gui.ActionEvent;
+import nl.stefsiekman.fps.gui.ActionListener;
 import nl.stefsiekman.fps.gui.Button;
 import nl.stefsiekman.fps.gui.MenuScreen;
 import nl.stefsiekman.fps.mapa.MapArchive;
 import nl.stefsiekman.fps.mapa.MapArchives;
 import nl.stefsiekman.fps.math.Vector2i;
 
-public class StateSingleplayerMenu extends MenuScreen{
+public class StateSingleplayerMenu extends MenuScreen implements ActionListener{
 	
 	private int maps = 0;
 	
@@ -20,9 +23,15 @@ public class StateSingleplayerMenu extends MenuScreen{
 			mapbutton.setLocation(new Vector2i(30, maps * 100 + 10));
 			mapbutton.setSize(100, 100);
 			mapbutton.setActionCommand(map.getName());
+			mapbutton.addActionListener(this);
 			
 			add(mapbutton);
 			maps += 1;
 		}
+	}
+
+	@Override
+	public void ActionPerformed(ActionEvent e){
+		LoadMap.loadMap(e.getActionCommand());
 	}
 }
