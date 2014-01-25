e478df16e3f4bb72a39969a281bba9bc6275a0ca
bartbart333 <bartholdbos@hotmail.com>
2014-01-25 16:58:49 +0100

    StateLoadMap now has a progressbar

--- src/nl/stefsiekman/fps/state/StateLoadMap.java
+++ src/nl/stefsiekman/fps/state/StateLoadMap.java
@@ -2,13 +2,20 @@ package nl.stefsiekman.fps.state;
 
 import java.io.IOException;
 
+import nl.stefsiekman.fps.Game;
 import nl.stefsiekman.fps.gui.MenuScreen;
+import nl.stefsiekman.fps.gui.ProgressBar;
 import nl.stefsiekman.fps.mapa.MapArchive;
+import nl.stefsiekman.fps.math.Vector2i;
 
 public class StateLoadMap extends MenuScreen{
 	
+	private ProgressBar progress = new ProgressBar();
+	
 	public StateLoadMap(){
+		progress.setLocation(new Vector2i(10, Game.HEIGHT - 10));
 		
+		add(progress);
 	}
 	
 	public void loadMap(MapArchive map){
