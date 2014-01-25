0449eb4d0c284f359923ec13fbce59df82543a48
SteFilms <s.siekman@gmail.com>
2014-01-25 13:22:47 +0100

    Frame count last second
    Added void that returns the amount of frames of the last second

--- src/nl/stefsiekman/fps/Time.java
+++ src/nl/stefsiekman/fps/Time.java
@@ -6,10 +6,22 @@ public class Time {
 	private static long lastTime = System.nanoTime();
 	private static double delta = 0.0;
 	
+	private static long lastSecondTime = System.currentTimeMillis();
+	private static int frames = 0;
+	private static int fps = 0;
+	
 	public static void updateDelta() {
 		long now = System.nanoTime();
 		delta = (now - lastTime) / SECOND;
 		lastTime = now;
+		
+		frames++;
+		long secondNow = System.currentTimeMillis();
+		if(secondNow - lastSecondTime >= 1000) {
+			fps = frames;
+			frames = 0;
+			lastSecondTime = secondNow;
+		}
 	}
 	
 	public static double getDelta() {
@@ -23,8 +35,15 @@ public class Time {
 	/*
 	 * Returns the most accurate calculation of the estimated frames per second
 	 */
-	public static float getFramesPerSecond() {
+	public static float getEstimatedFPS() {
 		return (float) (SECOND / delta);
+	}
+	
+	/*
+	 * Returns the amount of frames of the last second
+	 */
+	public static int getFPS() {
+		return fps;
 	}
 	
 }
